121273c9761bf8d100d0c2d1a1171b1623452f22
Tk 000407 <rodrigues.vini.2004@gmail.com>
2023-06-12 15:53:37 -0300

    Formulario

---
+++ stript.js
@@ -0,0 +1,31 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let departamento = document.getElementById('departamento').value;
+    let centro = document.getElementById('centro').value;
+
+    
+    if (username === '') {
+        alert('Por favor, digite o nome de usuário.');
+        
+    }
+
+    if (password === '') {
+        alert('Por favor, digite a senha.');
+        
+    }
+
+    if (departamento === '') {
+        alert('Por favor, selecione o departamento.');
+        
+    }
+
+    if (centro === '') {
+        alert('Por favor, selecione o centro.');
+        
+    }
+
+    this.submit();
+});
