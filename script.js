cc57cc3f884d917c202ac893e42d56bd7ffe56e2
Tk 000407 <rodrigues.vini.2004@gmail.com>
2023-07-04 11:50:32 -0300

    Update js

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 const deuErro = document.querySelector('.caixa-erro');
 
-function enviar(){
+function enviar() {
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
     const departamento = document.querySelector('#departamento').value;
     const centro = document.querySelector('#centro').value;
 
     if (username === '') {
-        deuErro.innerHTML = `Por favor, digite o nome de usuario.`; 
-        deuErro.classList.add('erro');   
-        deuErro.style.background = "red";    
+        deuErro.innerHTML = `Por favor, digite o nome de usuário.`;
+        deuErro.classList.add('erro');
+        deuErro.style.background = "red";
+    } else if (!isNaN(username)) {
+        deuErro.innerHTML = `Por favor, usar apenas letras.`;
+        deuErro.classList.add('erro');
+        deuErro.style.background = "red";
     } else if (password === '') {
         deuErro.innerHTML = `Por favor, digite a senha.`;
-        deuErro.classList.add('erro');   
-        deuErro.style.background = "red";    
+        deuErro.classList.add('erro');
+        deuErro.style.background = "red";
     } else if (departamento === '') {
-        deuErro.innerHTML = `Por favor, selecione o departamento.`; 
-        deuErro.classList.add('erro');   
-        deuErro.style.background = "red";     
-    } else if(centro === '') {
-        deuErro.innerHTML = `Por favor, selecione o centro.`;    
-        deuErro.classList.add('erro');  
-        deuErro.style.background = "red";   
+        deuErro.innerHTML = `Por favor, selecione o departamento.`;
+        deuErro.classList.add('erro');
+        deuErro.style.background = "red";
+    } else if (centro === '') {
+        deuErro.innerHTML = `Por favor, selecione o centro.`;
+        deuErro.classList.add('erro');
+        deuErro.style.background = "red";
     } else {
         deuErro.innerHTML = `Seja Bem-Vindo!`;
-        deuErro.classList.add('erro');     
+        deuErro.classList.add('erro');
         deuErro.style.background = "green";
     }
 }
@@ -34,5 +38,3 @@ function esqueci(){
     deuErro.classList.add('erro'); 
     deuErro.style.background = "blue"; 
 }
-
-
